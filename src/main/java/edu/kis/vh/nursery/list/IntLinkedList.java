db6171a428e317561b561c6d4426867efe10039f
Martyna Szymańska <245938@edu.p.lodz.pl>
2024-05-27 08:54:14 +0200

    14. usunięcie klasy Node.java oraz wpisanie ją do klasy IntLinedList.java

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -49,4 +49,31 @@ public class IntLinkedList {
         return i;
     }
 
+     static class Node {
+
+        private int value;
+        private Node prev;
+        public Node next;
+
+        public Node(int i) {
+            setValue(i);
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public void setValue(int value) {
+            this.value = value;
+        }
+
+        public Node getPrev() {
+            return prev;
+        }
+
+        public void setPrev(Node prev) {
+            this.prev = prev;
+        }
+    }
+
 }
