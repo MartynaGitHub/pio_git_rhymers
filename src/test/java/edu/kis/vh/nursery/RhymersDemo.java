27dd9608daf32aa28d47151598f2c11c256e2a3f
Martyna Szymańska <245938@edu.p.lodz.pl>
2024-05-27 08:45:36 +0200

    13. uporządkowanie RhymersDemo
    utworzenie metody testRhymers i rozbicie metody main

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -6,30 +6,33 @@ import edu.kis.vh.nursery.factory.RhymersFactory;
 class RhymersDemo {
 
     public static void main(String[] args) {
+        RhymersFactory factory = new DefaultRhymersFactory();
+        testRhymers(factory);
+    }
+
+    private static void testRhymers(RhymersFactory factory) {
         final int LOOP_INDEX_i = 15;
         final int LOOP_INDEX_j = 3;
-        RhymersFactory factory = new DefaultRhymersFactory();
-        
         DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
-        
+
         for (int i = 1; i < LOOP_INDEX_i; i++)
             for (int j = 0; j < LOOP_INDEX_j; j++)
                 rhymers[j].countIn(i);
-        
+
         java.util.Random rn = new java.util.Random();
         for (int i = 1; i < LOOP_INDEX_i; i++)
             rhymers[3].countIn(rn.nextInt(20));
-        
+
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
-        
+
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[3]).reportRejected());
-        
+
     }
 
 }
